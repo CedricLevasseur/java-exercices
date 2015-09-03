69b5328c9bbe5b4e141b2d5930af8b8198330c14
Cédric Levasseur <cedric.levasseur@cegedim.com>
2015-09-03 17:52:42 +0200

    First Version

---
+++ java-exercices/src/main/java/com/cedriclevasseur/javaexercices/generics/TreeNode.java
@@ -0,0 +1,62 @@
+package com.cedriclevasseur.javaexercices.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cedric Levasseur <cedric.levasseur@gmail.com>
+ */
+public class TreeNode<String,T> {
+    private List<TreeNode<String, T>> children = new ArrayList<TreeNode<String, T>>();
+    private String name;
+    private T value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public TreeNode(String name, T value) {
+        this.name = name;
+        this.value = value;
+    }
+    
+    public TreeNode(String name, T value, List<TreeNode<String,T>> children) {
+        this.name = name;
+        this.value = value;
+        this.children=children;
+    }
+    
+    
+
+    public List<TreeNode<String, T>> getChildren() {
+        return children;
+    }
+
+    public void addChild(TreeNode<String, T> child) {
+        this.children.add(child);
+    }
+    
+    public String show(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(name+"\n");
+        for(TreeNode<String,T> child: children){
+            sb.append("|--"+child.show());
+        }
+        return (String) sb;
+    }
+    
+    
+}
