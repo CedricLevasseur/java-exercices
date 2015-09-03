ec00054d01dc7971911d02b0382a8ba7ac7685f6
Cédric Levasseur <cedric.levasseur@cegedim.com>
2015-09-03 17:59:57 +0200

    first version

--- java-exercices/src/main/java/com/cedriclevasseur/javaexercices/GenericMain.java
+++ java-exercices/src/main/java/com/cedriclevasseur/javaexercices/GenericMain.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cedriclevasseur.javaexercices;
+
+import com.cedriclevasseur.javaexercices.generics.TreeNode;
 
 /**
  *
  * @author Cedric Levasseur <cedric.levasseur@gmail.com>
  */
 public class GenericMain {
+
+    TreeNode<String, String> root = new TreeNode<>("root", "0");
+    TreeNode<String, String> branch1 = new TreeNode<>("branch1", "1");
+    TreeNode<String, String> branch2 = new TreeNode<>("branch2", "2");
+    TreeNode<String, String> leaf1 = new TreeNode<>("leaf1", "3");
+    TreeNode<String, String> leaf2 = new TreeNode<>("leaf2", "4");
+    TreeNode<String, String> leaf3 = new TreeNode<>("leaf3", "5");
+    TreeNode<String, String> leaf4 = new TreeNode<>("leaf4", "6");
+    
+    root.addChild(branch1);
+    root.addChild(branch2);
+    
     
 }
